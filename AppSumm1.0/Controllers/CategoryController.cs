522821dfe1b1cd14f77763e151444376cd38b42d
Yura24061988 <42134407+Yura24061988@users.noreply.github.com>
2019-12-02 23:04:45 +0300

    Get,Post Create (#7)
    * Get,Post Create
    * ApiCorection

---
+++ AppSumm1.0/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppSumm1._0.Interface;
+using AppSumm1._0.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppSumm1._0.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public CategoryController(ICategories categories)
+        {
+            _categories = categories;
+        }
+        private readonly ICategories _categories;
+        [HttpGet]
+        [Route("categories")]
+        public IEnumerable<Category> GetCategories()
+        {
+            return _categories.GetCategories();
+        }
+        [HttpPost]
+        [Route("category")]
+        public void SetCategory(string category)
+        {
+            _categories.SetCategory(category);
+        }
+
+    }
+}
