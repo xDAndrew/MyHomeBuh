fa3d53efe54ce025ddbff17e1d603416e3abf045
Юрий <del24@mail.ru>
2019-12-02 22:28:44 +0300

    AddRepositoryCategory

--- AppSumm1.0/Interface/IRepository.cs
+++ AppSumm1.0/Interface/IRepository.cs
@@ -1,10 +1,13 @@
 ﻿using AppSumm1._0.Model;
+using System.Collections.Generic;
 
 namespace AppSumm1._0.Interface
 {
     public interface IRepository
-     {
+    {
         void SaveHistory(DbModel dbModel);
         DbModel LoadHistory();
-     }
+        void SaveCategories(List<Category> category);
+        IEnumerable<Category> LoadCategories();
+    }
 }
--- AppSumm1.0/Service/Repository.cs
+++ AppSumm1.0/Service/Repository.cs
@@ -2,12 +2,14 @@
 using System.IO;
 using Newtonsoft.Json;
 using AppSumm1._0.Model;
+using System.Collections.Generic;
 
 namespace AppSumm1._0
 {
     public class Repository : IRepository
     {
         private const string path = "test.json";
+        private const string pathDb = "categories.json";
 
         public DbModel LoadHistory()
         {
@@ -16,14 +18,30 @@ namespace AppSumm1._0
                 return JsonConvert.DeserializeObject<DbModel>(File.ReadAllText(path));
             }
             else
-            { 
-                return new DbModel(); 
+            {
+                return new DbModel();
             }
         }
 
         public void SaveHistory(DbModel dbModel)
         {
             File.WriteAllText(path, JsonConvert.SerializeObject(dbModel));
+        }
+        public void SaveCategories(List<Category> category)
+        {
+            File.WriteAllText(pathDb, JsonConvert.SerializeObject(category));
+            
+        }
+        public IEnumerable<Category> LoadCategories()
+        {
+            if (File.Exists(pathDb))
+            {
+                return JsonConvert.DeserializeObject<IEnumerable<Category>>(File.ReadAllText(pathDb));
+            }
+            else
+            {
+                return new List<Category>();
+            }
         }
     }
 }
