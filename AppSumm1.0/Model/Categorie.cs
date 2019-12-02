286b245265bbc32bd568cbc1e2a028dcba5cdfdd
Юрий <del24@mail.ru>
2019-12-02 20:04:01 +0300

    AddCategorie

---
+++ AppSumm1.0/Model/Categorie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppSumm1._0.Model
+{
+    public class Categorie
+    {
+        public int Id { get; set; }
+        public string CategorieName { get; set; }
+    }
+}
---
+++ AppSumm1.0/Service/Categories.cs
@@ -0,0 +1,25 @@
+﻿using AppSumm1._0.Interface;
+using AppSumm1._0.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppSumm1._0.Service
+{
+    public class Categories : ICategorie
+    {
+        private List<Categorie> categories;
+        public List<Categorie> GetCategories()
+        {
+            return categories;
+        }
+        public void SetCategories(Categorie categorie)
+        {
+            categories.Add(new Categorie()
+            {
+                CategorieName = categorie.CategorieName
+            });
+        }
+    }
+}
